5b9b27d8ded4dabc285e0f3a05e5a7b95ed0dadd
Victor Porto Saar <victorpsaar@gmail.com>
2024-07-24 09:40:41 -0300

    Commit inicial

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multi(a, b) {
+    return a * b;
+}
+function concat(nome) {
+    return `Olá ${nome}! Como você está?`;
+}
+console.log(multi(10, 20));
+console.log(concat('Rafael'));
---
+++ main.ts
@@ -0,0 +1,10 @@
+function multi(a: number, b:number): number {
+    return a * b;
+}
+
+function concat(nome: string): string {
+    return `Olá ${nome}! Como você está?`
+}
+
+console.log(multi(10,20));
+console.log(concat('Rafael'));
